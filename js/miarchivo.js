179866a180db7270ed81448bd3226fd0bdc9aea9
niederhausertomas <78279996+niederhausertomas@users.noreply.github.com>
2022-09-19 08:58:28 -0300

    .

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -274,7 +274,7 @@ function flete(){
                     <div class="card mb-3" style="width: 95%; height: 195px;">
             <div class="row g-0">
                 <div class="col-md-4">
-                <svg xmlns="http://www.w3.org/2000/svg" width="140px" height="140px" fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="160px" height="160px"  fill="currentColor" class="bi bi-truck" viewBox="0 0 16 16">
                 <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                 </svg></div>
                 <div class="col-md-8">
@@ -291,46 +291,3 @@ function flete(){
             `
         document.getElementById("cardFlete").innerHTML=contenidoFlete;
 }
-
-/*
-function respuestaChango(){
-    actualizarCarrito();
-    opcion=1;
-    while(opcion!=0){
-        opcion = parseInt(prompt("Desea contratar flete para el envio: \n 1- si.  2- no."));
-        switch (opcion) {
-        case 1:
-            opcion=0;    
-            let km=parseInt(prompt("Ingrese los km de distancia a recorrer por el flete."));
-            flete=km*300;
-            monto+=flete;
-            console.log("Gasto de envio: " + flete);
-        break; 
-        case 2:
-            opcion=0;
-            break; 
-        default:
-            cartelError();
-            break;
-        }
-    }
-*/
-/*
-    function descuento(monto, descuento){
-        monto=monto-(monto*descuento/100);
-        return monto;
-    }
-    
-    function formaDePago(opcion){
-        opcion === 1 && (monto = descuento(monto,20));
-        opcion === 2 && (monto=descuento(monto,10));
-        opcion === 3 && (monto=monto*1.15);
-        opcion === 3 && (cuota=(monto/2));
-        opcion === 3 && (console.log("Total a pagar: $ " + monto + " en dos cuotas de $" + cuota ));
-    }
-    opcion = parseInt(prompt("Ingrese la forma en la que desea pagar: \n 1- Efectivo 20% de descuento. \n 2- Tarjeta en un pago 10% de descuento \n 3- Tarjeta en dos pagos (15% de interes)"));
-    formaDePago(opcion);
-}
-
-
-*/
